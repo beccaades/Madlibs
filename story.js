859db53b4661810358bc78b275ad82c951e16fa7
beccaades <rebecca.ades@gmail.com>
2015-09-11 18:29:15 -0400

    american beauty mad libs finished needs styling

--- story.js
+++ story.js
@@ -1,29 +1,29 @@
 var adjective = prompt('Please type in an adjective');
 string = "<h2> Do you want to see the most " + adjective + " thing I've ever filmed?";
 var times = prompt('Please enter a time of day');
-sentence += " It was one of those " + times;
+sentence = " It was one of those " + times;
 var verb = prompt('Please enter a past participle verb (verb ending in -ING');
-sentence += " when it's a minute away from " + verb;
+sentence += "'s when it's a minute away from " + verb;
 var secondVerb = prompt('Please enter a verb');
-sentence += " and there's this electricity in the air, you can almost" + anotherVerb;
+sentence += " and there's this electricity in the air, you can almost " + secondVerb;
 var noun = prompt('please enter a noun');
-sentence += " hear it. And this " + noun;
-var thirdVerb = prompt("Please enter a past participle (you couldn't have forgotten already...");
+sentence += " it. And this " + noun;
+var thirdVerb = prompt("Please enter a past participle (you couldn't have forgotten already...)");
 sentence += " was just, " + thirdVerb;
 var secondNoun = prompt('please enter a noun')
 sentence += " with me, like a little " + secondNoun;
 var fourthVerb = prompt('Please enter a verb');
 sentence += " kid beggin' me to " + fourthVerb;
 var secondTime = prompt('please enter a duration of time');
-sentence += "with it - for " + secondTime + ".";
+sentence += " with it - for " + secondTime + ".";
 var thirdNoun = prompt('please enter a noun');
 sentence += " And that's the day I realized that there was this entire " + thirdNoun;
 var emotion = prompt('please enter an emotion');
-sentence += "behind things, and this incredibly benevolent force that wanted me to know that there was no reason to be " + emotion;
+sentence += " behind things, and this incredibly benevolent force that wanted me to know that there was no reason to be " + emotion;
 var secondAdjective = prompt('Please type in an adjective');
 sentence += ", ever. Video's a poor excuse, I know. But it helps me remember - I need to remember. Sometimes, there's so much " + secondAdjective;
 var bodyPart = prompt('please enter a body part');
 alert("Ready to see your Mad Libs?");
-sentence += "beauty in the world - I feel like I can't take it, like my " + bodyPart + " is just going to cave in.</h2>";
+sentence += " in the world - I feel like I can't take it, like my " + bodyPart + " is just going to cave in.</h2>";
 document.write(sentence);
 
