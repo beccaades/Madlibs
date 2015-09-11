1117ee95af82345e26d62fa609fb00fffbde66a1
beccaades <rebecca.ades@gmail.com>
2015-09-11 18:07:33 -0400

    adding madlib prompts

---
+++ story.js
@@ -0,0 +1,25 @@
+var adjective = prompt('Please type in an adjective');
+string = "<h2> Do you want to see the most " + adjective + " thing I've ever filmed?";
+var times = prompt('Please enter a time of day');
+sentence += " It was one of those " + times;
+var verb = prompt('Please enter a past participle verb (verb ending in -ING');
+sentence += " when it's a minute away from " + verb;
+var secondVerb = prompt('Please enter a verb');
+sentence += " and there's this electricity in the air, you can almost" + anotherVerb;
+var noun = prompt('please enter a noun');
+sentence += " hear it. And this " + noun;
+var thirdVerb = prompt("Please enter a past participle (you couldn't have forgotten already...");
+sentence += " was just, " + thirdVerb;
+var secondNoun = prompt('please enter a noun')
+sentence += " with me, like a little " + secondNoun;
+var fourthVerb = prompt('Please enter a verb');
+sentence += " kid beggin' me to " + fourthVerb;
+var secondTime = prompt('please enter a duration of time');
+sentence += "with it - for " + secondTime + ".";
+var thirdNoun = prompt('please enter a noun');
+sentence += " And that's the day I realized that there was this entire " + thirdNoun;
+var emotion = prompt('please enter an emotion');
+sentence += 
+
+behind things, and this incredibly benevolent force that wanted me to know that there was no reason to be afraid, ever. Video's a poor excuse, I know. But it helps me remember - I need to remember. Sometimes, there's so much beauty in the world - I feel like I can't take it, like my heart is just going to cave in."
+
